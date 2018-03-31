d1a8d7f4e61f5205d1489629c7777d2deca7956b
Анатолий Серёгин <aseregin@outlook.com>
2018-03-31 23:22:14 +0300

    Added compability for legacy devices and working_dir definition

--- check_scripts/check_lsimegaraid.ps1
+++ check_scripts/check_lsimegaraid.ps1
@@ -1,11 +1,21 @@
-﻿# Check MegaRaid controller health status
-# MegaRAID Storage Manager should be installed in C:\Program Files (x86)\MegaRAID Storage Manager
+﻿<#
+.SYNOPSIS
+    Check MegaRaid controller health status.
+.DESCRIPTION
+    MegaRAID Storage Manager should be installed in C:\Program Files (x86)\MegaRAID Storage Manager. It works through StorCli by default.
+    You can use MegaCli for legacy devices.
+.PARAMETER Legacy
+    If default mode isn't working try set this to $true
+# 
+#>
 
 [CmdletBinding()]
 Param(
-
+    [Parameter()]
+    [boolean]$Legacy = $false
 )
 
+$working_dir = Split-Path -path $MyInvocation.MyCommand.Path -Parent
 $t = $host.ui.RawUI.ForegroundColor
 $state = 0
 $states_text = @('ok', 'warning', 'critical', 'unknown')
@@ -13,7 +23,12 @@ $state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
 $bad_controllers = @()
 $output = ''
 
-if (test-path "C:\Program Files (x86)\MegaRAID Storage Manager\StorCLI64.exe") {
+if ($Legacy) {
+    $data = Invoke-Expression -Command "$working_dir\vendor\lsimegaraid\MegaCli.exe -LDInfo -LAll -aAll" | ? {$_ -match "State\s*:"} | ? {$_ -notmatch "Optimal"}
+    if ($data) {
+        $state = 2
+    }
+} elseif (Test-path "C:\Program Files (x86)\MegaRAID Storage Manager\StorCLI64.exe") {
     $data = (Invoke-Expression -Command "& 'C:\Program Files (x86)\MegaRAID Storage Manager\StorCLI64.exe' show J") -join "`n" | ConvertFrom-Json
 
     if ($data.Controllers."Command Status".Status -and $data.Controllers."Response Data".'Number of Controllers') {
@@ -24,6 +39,8 @@ if (test-path "C:\Program Files (x86)\MegaRAID Storage Manager\StorCLI64.exe") {
                 $state = 2
             }
         }
+    } else {
+        $state = 3
     }
 
     if ($bad_controllers) {
