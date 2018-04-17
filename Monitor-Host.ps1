6279052f4bc36e2d4278b92020e149cbb9580fdf
Анатолий Серёгин <aseregin@outlook.com>
2018-04-17 17:03:34 +0300

    Added passive check autocreation on first startup

--- Monitor-Host.ps1
+++ Monitor-Host.ps1
@@ -1,11 +1,62 @@
 ﻿# Host monitoring and sending data to monopus.io
+Function Get-Services {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        $Config
+    )
+    process {
+        $settings_obj = (Invoke-WebRequest $config.uri -Method Post -UseBasicParsing -Body @{api_key=$($config.api_key);id=$($config.id);mon_action='check/status';class="host"}).content | ConvertFrom-Json
+        $services = @{}
+        if ($settings_obj -and $settings_obj.data.services) {
+            ($settings_obj.data.services).psobject.properties  | % {$services[$_.Name] = $_.Value}
+        }
+        return $services
+    }
+}
+
+
+$max_retries = 6
+$retry_interval = 10
+$work = $false
+$config_path = "${env:ProgramFiles(x86)}\MonOpus\main.cfg"
 
 try {
-    $config = iex (Get-Content -Raw -Path "${env:ProgramFiles(x86)}\MonOpus\main.cfg")
-    $settings_obj = (Invoke-WebRequest $config['uri'] -Method Post -UseBasicParsing -Body @{api_key=$config['api_key'];id=$config['id'];mon_action='check/status';class="host"}).content | ConvertFrom-Json
-    $services = @{}
-    ($settings_obj.data.services).psobject.properties  | % {$services[$_.Name] = $_.Value}
-    $work = $true
+    $config = Get-Content -Raw -Path $config_path | ConvertFrom-Json
+
+    if (!$config.id) {
+        $op_system = gwmi win32_operatingsystem
+        $data = @{
+            'name' = (hostname);
+            'address' = (Invoke-RestMethod http://ipinfo.io/json | Select -exp ip);
+            'os' = $op_system.caption;
+            'memory' = (('{0:N0}' -f ($op_system.TotalVisibleMemorySize / 1Mb)) + 'Gb');
+            'api_key' = $config.api_key;
+            'mon_action' = 'site/create';
+            'drives' = ((gwmi win32_logicaldisk | ? {$_.drivetype -eq 3}).deviceid -replace ':') -join ','
+        }
+
+        $response = Invoke-RestMethod $config.uri -Method Post -UseBasicParsing -Body $data
+
+        $config | Add-Member @{id=$($response.data.id)} -PassThru -Force | Out-Null
+        $config | ConvertTo-Json | Set-Content -Path $config_path
+    }
+
+    # TO DO: Add check for several time before stopping because checks might be created later
+    for ($i=1; $i -le $max_retries; $i++) {
+        if ($services = Get-Services -Config $config) {
+            $work = $true
+            break;
+        } else {
+            if ($i -eq $max_retries) {
+                Write-Output "There are no services for this host"
+                exit
+            } else {
+                sleep -Seconds $retry_interval
+            }
+        }
+    }
+    
 } catch {
     Write-Output ("Error on init stage: " + $_)
     exit 1
@@ -23,12 +74,12 @@ while ($work) {
         if ($services[$key].active -and $services[$key].passive -and $services[$key].command -and ($updatedon+$services[$key].interval*60) -lt $timestamp) {
 
             $args = if ($services[$key].args) {$services[$key].args -replace '=', ' '} else {''}
-            $command = ($config['scripts_path'] + $services[$key].command + ".ps1")
+            $command = ($config.scripts_path + $services[$key].command + ".ps1")
             $w = if($services[$key].warning) {"-w $($services[$key].warning)"} else {}
             $c = if($services[$key].critical) {"-c $($services[$key].critical)"} else {}
             
             $result = iex "& `"$command`" $args $w $c"
-            $r = Invoke-WebRequest $config['uri'] -Method Post -UseBasicParsing -Body @{api_key=$config['api_key'];id=$services[$key].id;mon_action='check/handle_result';result=$result;state=$lastexitcode}
+            $r = Invoke-WebRequest $config.uri -Method Post -UseBasicParsing -Body @{api_key=$($config.api_key);id=$services[$key].id;mon_action='check/handle_result';result=$result;state=$lastexitcode}
             $response = $r.content | ConvertFrom-Json
             Write-Verbose $response
             
@@ -40,6 +91,11 @@ while ($work) {
                     $services[$key].updatedon = $response.data.updatedon
                     $services[$key].active = $response.data.active
                     $services[$key].interval = if ($response.data.interval) {$response.data.interval} Else {1}
+
+                    # Get new services
+                    if ($response.data.newservices) {
+                        $services = Get-Services -Config $config
+                    }
                 }
             }
         }
