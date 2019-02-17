7282ba041d306ccab0e005e8c475277d8a701163
Анатолий Серёгин <aseregin@outlook.com>
2019-02-17 16:21:09 +0300

    Added Azure DPM jobs monitoring

---
+++ check_scripts/dpm_jobs.ps1
@@ -0,0 +1,19 @@
+﻿[CmdletBinding()]
+Param(
+)
+$output = $null
+$perf = $null
+$states_text = @('ok', 'warning', 'critical')
+$state = 0
+
+if ($alerts = (Get-DPMAlert | ? {$_.Severity -match "Error|Warning"})) {
+    $servers = $alerts.server -join ','
+    $issues = ($alerts.DetailedErrorInfo -split ';')[0] -join ','
+    $output = "servers==$($servers)__issues==$($issues)"
+    $state = 2
+}
+
+$output = "dpm_jobs_$($states_text[$state])::$output | $perf"
+Write-Verbose $output
+Write-Output $output
+exit $state
