51ad4fe1ba95fcba449c01300a9fd0ab26f54a82
Анатолий Серёгин <aseregin@outlook.com>
2019-03-15 22:57:46 +0300

    If disk is not found raise error

--- check_scripts/local_disk.ps1
+++ check_scripts/local_disk.ps1
@@ -1,4 +1,5 @@
 ﻿[CmdletBinding()]
+
 Param(
   [Parameter()]
    [string]$W = 10,
@@ -7,17 +8,21 @@ Param(
   [Parameter()]
    [string]$drive = 'C'
 )
+
 $output = $null
 $perf = $null
-$states_text = @('ok', 'warning', 'critical')
+$t = $host.ui.RawUI.ForegroundColor
+$state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
+$states_text = @('ok', 'warning', 'critical', 'unknown')
 $state = 0
 $W = $W -replace "[^0-9]"
 $C = $C -replace "[^0-9]"
-$drive = $drive + ':'
 
-$volumes = gwmi Win32_LogicalDisk -Filter "DriveType=3 and DeviceID='$drive'"
-Write-Verbose $volumes
-foreach ($vol in $volumes) {
+Write-Verbose "Проверяем диск $($drive):"
+$vol = gwmi Win32_LogicalDisk -Filter "DriveType=3 and DeviceID='$($drive):'"
+
+if ($vol) {
+
   $used_units = $vol.Size - $vol.FreeSpace
   $used_pct = [math]::Round($used_units*100/$vol.Size)
   $free_pct = [math]::Round($vol.FreeSpace*100/$vol.Size)
@@ -31,7 +36,7 @@ foreach ($vol in $volumes) {
     $free_units = "$([math]::Round($vol.FreeSpace/1Tb, 1))Tb"
   }
 
-  $output += "dev==$($disk_name)__free_units==$($free_units)__dused_units==$($used_units)__dused_pct==$($used_pct)"
+  $output += "::dev==$($disk_name)__free_units==$($free_units)__dused_units==$($used_units)__dused_pct==$($used_pct)"
   $perf += $disk_name + '=' + [math]::Round($used_units/1Mb) + "MB;" + [math]::Round($vol.Size*$w/100/1MB) + ';' + [math]::Round($vol.Size*$c/100/1MB) + ';0;' + [math]::Round($vol.Size/1Mb) + ' '
   if ($w -and $c) {
       if ($free_pct -le $W -and $free_pct -gt $C) {
@@ -40,8 +45,15 @@ foreach ($vol in $volumes) {
         $state = 2
       }
   }
+} else {
+    $state = 2
+    $output = "_not_found::dev==$($drive)"
 }
-$output = "local_disk_$($states_text[$state])::$output | $perf"
+
+$output = "local_disk_$($states_text[$state])$output | $perf"
+
 Write-Verbose $output
+$host.ui.RawUI.ForegroundColor = $($state_colors[$state])
 Write-Output $output
+$host.ui.RawUI.ForegroundColor = $t
 exit $state
