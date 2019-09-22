9b819812ad4eff4419490ea0e2644bd322fe760f
Анатолий Серёгин <aseregin@outlook.com>
2019-09-22 23:16:38 +0300

    Добавлена проверка успешности заданий по расписанию

---
+++ check_scripts/get_schtaskstatus.ps1
@@ -0,0 +1,56 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true,
+               Position=0,
+               ValueFromPipelineByPropertyName=$true,
+               ParameterSetName='Parameter Set 1')]
+  [ValidateNotNullOrEmpty()]
+  [string[]]$JobName
+)
+
+Begin {
+    $output = $null
+    $perf = $null
+    $t = $host.ui.RawUI.ForegroundColor
+    $states_text = @('ok', 'warning', 'critical', 'unknown')
+    $state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
+    $state = @()
+    $bad_jobs = 0
+    $jobs = @{
+        'good' = @();
+        'bad' = @();
+        'unknown' = @();
+    }
+}
+
+
+Process {
+    foreach($job in $JobName){
+        if ($task = Get-ScheduledTaskInfo $job -ErrorAction SilentlyContinue) {
+            if ($task.LastTaskResult) { 
+                $state += 2
+                $jobs['bad'] += $job
+            } else {
+                $state += 0
+                $jobs['good'] += $job
+            }
+        } else {
+            $state += 3
+            $jobs['unknown'] += $job
+        }
+    }
+}
+
+End {
+    $state = ($state | measure -Maximum).Maximum
+    $perf = "bad_jobs=" + ($jobs['bad'].count + $jobs['unknown'].count) + ';1;1;0;' + $JobName.count
+    $output = "good==" + ($jobs['good'] -join ',') + "__bad==" + ($jobs['bad'] -join ',') + "__unknown==" + ($jobs['unknown'] -join ',')
+    $output = "get_schtaskstatus_$($states_text[$state])::$output | $perf"
+    Write-Verbose $output
+    $host.ui.RawUI.ForegroundColor = $($state_colors[$state])
+    Write-Output $output
+    $host.ui.RawUI.ForegroundColor = $t
+    exit $state
+}
+
+
