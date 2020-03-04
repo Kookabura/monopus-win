4ccf8b1464f660e4edd54e377a9237a6d4635ba0
Анатолий Серёгин <aseregin@outlook.com>
2020-03-04 15:04:05 +0300

    Switched to RdpCoreTS Operational log to determine bad logon attempts

--- action_scripts/Block-RDPBruteForce.ps1
+++ action_scripts/Block-RDPBruteForce.ps1
@@ -3,21 +3,11 @@ Import-Module "International"
 Import-Module "NetSecurity"
 
 
-$start_time_event_search = (Get-Date).AddMinutes(-5)
-$current_locale=(Get-WinSystemLocale).Name
-$time_span=(New-TimeSpan -Start $start_time_event_search -End (get-date)).Minutes
+$start_time_event_search = (Get-Date).AddMinutes(-10) # Period where we're looking for bad attempts
+$max_bad_attempts = 5 # Max bad logon attempts
 $firewall_rule_name="BanIP"
 $local_port=-1 #0 is mean all protocols, 1-65535 - block this TCP and UDP ports
 $ip_address_list=@()
-
-
-if ($current_locale -eq 'ru-RU') {
-    $match_pattern='Тип входа'
-} else {
-    $match_pattern='Logon type'
-}
-
-$match_pattern+=':\s+(3)\s'
 
 if ($local_port -eq -1) {
     $local_port=(Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -name PortNumber).PortNumber
@@ -27,26 +17,37 @@ if ($local_port -eq -1) {
 }
 
 # Getting bad logons attempts
-$bad_RDP_logons = Get-EventLog -LogName 'Security' -After $start_time_event_search -InstanceId 4625 | ? {$_.Message -match $match_pattern} | select @{n='IpAddress';e={$_.ReplacementStrings[-2]} }
-$get_ip = ($bad_RDP_logons | group -property IpAddress | ? {$_.Count -gt 5}).Name
+$get_ip=@()
+$events = Get-WinEvent -FilterHashtable @{LogName='Microsoft-Windows-RemoteDesktopServices-RdpCoreTS/Operational'; StartTime=$start_time_event_search; ID=140}
 
+foreach ($e in $events) {
+    $exml = [xml]$e.toXML()
+    $get_ip += $exml.Event.EventData.Data.'#text'
+}
+$get_ip = ($get_ip | group | ? {$_.count -ge $max_bad_attempts}).name
+
+if (!$get_ip) {
+    Write-Host "There is no bad ip address. Stopping..."
+    exit
+}
 
 # Getting currently blocked IP addresses
 if (Get-NetFirewallRule -DisplayName $firewall_rule_name -ErrorAction SilentlyContinue) {
-    foreach ($ip in (Get-NetFirewallRule -DisplayName $firewall_rule_name | Get-NetFirewallAddressFilter).RemoteAddress | select -Unique) {$ip_address_list+=$ip}
+    foreach ($ip in (Get-NetFirewallRule -DisplayName $firewall_rule_name | Get-NetFirewallAddressFilter).RemoteAddress | select -Unique) {
+        $ip_address_list+=$ip
+    }
 }
 $new_ip_address_list = $ip_address_list
 
-
 # Populate new IPs for blocking
 foreach ($ip in $get_ip) { 
-    
+
     if ($ip_address_list -notcontains $ip) {
      
         Write-Verbose "Adding $ip to RDP block list"
         $new_ip_address_list += $ip
         
-        $log_body='IP ' + $ip + ' заблокирован за ' + ($bad_RDP_logons | ? {$_.IpAddress -eq $ip}).Count + " попыток входа в систему за $time_span минут"
+        $log_body='IP ' + $ip + ' was banned for ' + $max_bad_attempts + " or greater bad logon attempts since $start_time_event_search"
 
         Write-EventLog -LogName 'System' -Source 'System' -Message $log_body -EventId 777 -EntryType Warning
     }
@@ -55,7 +56,7 @@ foreach ($ip in $get_ip) {
 # Update firewall rules only if we have new IPs for blocking and ip list is not empty
 if ($new_ip_address_list -ne $ip_address_list -and $new_ip_address_list) {
 
-    if (!(Get-NetFirewallRule -DisplayName $firewall_rule_name)) {
+    if (!(Get-NetFirewallRule -DisplayName $firewall_rule_name -ErrorAction SilentlyContinue)) {
 
         if ($local_port -eq 0) {
             New-NetFirewallRule -DisplayName $firewall_rule_name –RemoteAddress $new_ip_address_list -Direction Inbound -Action Block
@@ -70,6 +71,5 @@ if ($new_ip_address_list -ne $ip_address_list -and $new_ip_address_list) {
 
     }
 }
-
 
 #(Get-NetFirewallRule -DisplayName $firewall_rule_name | Get-NetFirewallAddressFilter).RemoteAddress | sort
