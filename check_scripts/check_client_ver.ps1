b3ff042467abcaf1079dade2c385cf3d13ecf4ef
Anatoly Seregin <aseregin@tolkit.top>
2021-01-03 23:59:19 +0300

    Moved update config logic to Update-MonOpusClient function to use it separately from check

--- check_scripts/check_client_ver.ps1
+++ check_scripts/check_client_ver.ps1
@@ -5,52 +5,62 @@ Param(
 )
 
 Begin {
-        # Return $false or installed version number
-        Function Update-MonOpusClient {
-            [CmdletBinding()]
-            Param(
-                [Parameter(Mandatory=$true)]
-                $Config,
-                [Parameter(Mandatory=$true)]
-                [System.Version]$NewVersion
+    # Return $false or installed version number
+    Function Update-MonOpusClient {
+        [CmdletBinding()]
+        Param(
+            [Parameter(Mandatory=$true)]
+            $Config,
+            [Parameter(Mandatory=$true)]
+            [System.Version]$NewVersion
                 
-            )
-            process {
+        )
+        process {
 
-                try {
+            try {
 
-                    if (Test-Path $Config.installation_path) {
+                if (Test-Path $Config.installation_path) {
                         
-                        Try { [io.file]::OpenWrite("$($Config.installation_path)\file.tmp").close() }
-                        Catch { 
-                            Write-Warning "Unable to write to output file $outputfile"
-                            return $false
-                        }
-
-                        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-                        Invoke-WebRequest "https://github.com/Kookabura/monopus-win/archive/$NewVersion.zip" -OutFile "$env:TEMP\monopus-win.zip" -UseBasicParsing
-
-                        if (Test-Path "$env:TEMP\monopus-win-$NewVersion") {
-                            rm "$env:TEMP\monopus-win-$NewVersion" -Force -Recurse
-                        }
-
-                        Add-Type -AssemblyName System.IO.Compression.FileSystem
-                        [System.IO.Compression.ZipFile]::ExtractToDirectory("$env:TEMP\monopus-win.zip", $env:TEMP)
-
-                        cp "$env:TEMP\monopus-win-$NewVersion\*" $Config.installation_path -Recurse -Force -Exclude 'main.cfg'
-                        $Config | ConvertTo-Json -Compress | Set-Content -Path "$($Config.installation_path)\main.cfg"
-
-                        Write-Verbose "Client successfully updated"
-
-                        return $NewVersion
+                    Try { [io.file]::OpenWrite("$($Config.installation_path)\file.tmp").close() }
+                    Catch { 
+                        Write-Warning "Unable to write to output file $outputfile"
+                        return $false
                     }
-                } catch {
-                    Write-Error ("Error on update client stage: " + $_)
-                }
 
-                return $false
+                    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+                    Invoke-WebRequest "https://github.com/Kookabura/monopus-win/archive/$NewVersion.zip" -OutFile "$env:TEMP\monopus-win.zip" -UseBasicParsing
+
+                    if (Test-Path "$env:TEMP\monopus-win-$NewVersion") {
+                        rm "$env:TEMP\monopus-win-$NewVersion" -Force -Recurse
+                    }
+
+                    Add-Type -AssemblyName System.IO.Compression.FileSystem
+                    [System.IO.Compression.ZipFile]::ExtractToDirectory("$env:TEMP\monopus-win.zip", $env:TEMP)
+
+                    cp "$env:TEMP\monopus-win-$NewVersion\*" $Config.installation_path -Recurse -Force -Exclude 'main.cfg'
+
+                    $config_sample = Get-Content "$($Config.installation_path)\main.cfg.sample" -Raw | ConvertFrom-Json
+                    foreach ($prop in $config_sample.psobject.Properties) {
+                        if(!(Get-Member -inputobject $config -name $prop.name -Membertype Properties)){
+                            #Property not exists
+                            $config | Add-Member -MemberType NoteProperty -Name $prop.name -Value $config_sample.$($prop.name)
+                        }
+                    }
+
+                    $config | Add-Member -MemberType NoteProperty -Name 'version' -Value $NewVersion.ToString() -Force
+                    $Config | ConvertTo-Json -Compress | Set-Content -Path "$($Config.installation_path)\main.cfg"
+
+                    Write-Verbose "Client successfully updated"
+
+                    return $NewVersion
+                }
+            } catch {
+                Write-Error ("Error on update client stage: " + $_)
             }
+
+            return $false
         }
+    }
 }
 
 Process {
@@ -65,8 +75,6 @@ Process {
     if ($latest_version -gt [System.Version]$config.version) {
         if ($v = Update-MonOpusClient -Config $Config -NewVersion $latest_version) {
             
-            $config | Add-Member @{version=$v.ToString()} -PassThru -Force | Out-Null
-            $Config | ConvertTo-Json -Compress | Set-Content -Path "$($Config.installation_path)\main.cfg"
             cmd /c "SCHTASKS /End /TN $($config.task_name) && SCHTASKS /Run /TN $($config.task_name)"
         } else {
             $state = 1
