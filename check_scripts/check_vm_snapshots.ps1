8c7117d41c887ce35d9bc0779c4736e8e3d8e054
Maksim <86305698+Maks19881@users.noreply.github.com>
2021-07-14 09:04:00 +0300

    Add files via upload

---
+++ check_scripts/check_vm_snapshots.ps1
@@ -0,0 +1,20 @@
+﻿[CmdletBinding()]
+Param(
+)
+$output = $null
+$perf = $null
+$states_text = @('ok', 'warning', 'critical')
+$state = 0
+
+$snapshots = Get-VMSnapshot –VMname * | ? {$_.CreationTime -lt (Get-Date).AddDays(-1)}
+$snap_count = $snapshots.count
+
+if ($snap_count) 
+    {
+        $state = "2"
+    }
+
+$output = "vm_snapshots_$($states_text[$state])::snapshots==$snap_count | snapshots=$snap_count;;;;;"
+Write-Verbose $output
+Write-Output $output
+exit $state
