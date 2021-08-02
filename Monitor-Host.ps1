deefec57b0148b1bd018e3f4615f0cb1de87063a
Anatoly Seregin <aseregin@tolkit.top>
2021-08-03 00:46:41 +0300

    Added support of different TLS levels on server side

--- Monitor-Host.ps1
+++ Monitor-Host.ps1
@@ -35,7 +35,7 @@
         }
 
         Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-
+        [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 
     }
 
