c1516cf25ec18c1fa6068481e1977d1df6f37828
Maksim <86305698+Maks19881@users.noreply.github.com>
2021-08-05 05:54:41 +0300

    Add files via upload

---
+++ check_scripts/Check_Java_x64.ps1
@@ -0,0 +1,18 @@
+﻿[CmdletBinding()]
+Param(
+)
+$output = $null
+$perf = $null
+$states_text = @('ok', 'warning', 'critical')
+$state = 0
+
+$Path = Test-Path -Path 'C:\Program Files\Java'
+If ($Path -eq 'True')
+    {
+        $state = "2"
+    }    
+
+$output = "Path$($states_text[$state])::Events=='обнаружена Java x64' | Events='обнаружена Java x64';;;;;"
+Write-Verbose $output
+Write-Output $output
+exit $state
