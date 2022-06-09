2a68c34358f0b349a1bec826e9bc4643848d34e6
Anrikko <79712453+Anrikko@users.noreply.github.com>
2022-06-09 17:05:01 +0300

    Updated script

--- check_scripts/check_policy_passwordComplexity.ps1
+++ check_scripts/check_policy_passwordComplexity.ps1
@@ -1,16 +1,10 @@
 [CmdletBinding()]
-<#Param
-(
-	[Parameter()][string[]] 
-	[Parameter()][int]
-)#>
-
 $t = $host.ui.RawUI.ForegroundColor
 $states_text = @('ok', 'critical', 'unknown')
 $state_colors = @('Green', 'Red', 'DarkGray')
 $state = 0
-$PasswordComplexity = "on"
-$LockoutBadCount = "on"
+$err = ""
+$errmsg = "ok"
 
 try
 {
@@ -39,9 +33,10 @@ try
 					{
 						$state = 1
 						Write-Host "No Lockout Policy" -ForegroundColor Red
-						$LockoutBadCount = "off"
+						$err = "_lockout"
 					}
 				}
+				
 				if($policy -like "PasswordComplexity")
 				{
 					$value = $line.substring($line.IndexOf("=") + 1,$line.Length - ($line.IndexOf("=") + 1))
@@ -50,22 +45,22 @@ try
 					{
 						$state = 1
 						Write-Host "No Strong Password Policy" -ForegroundColor Red
-						$PasswordComplexity = "off"
+						$err = "_strong_password"
 					}
 				}
 			}
 		}
 		Remove-Item $file
+		if ($err -ne "") {$errmsg = "err" + $err}
 	}
 }
 catch { 
 	Write-Host $_ -ForegroundColor Red
 	$state = 2
-	$PasswordComplexity = "unknown"
-	$LockoutBadCount = "unknown"
+	$errmsg = "err"
 }
 	
-$output = "check_status.$($states_text[$state]) | PasswordComplexity=$($PasswordComplexity);;; | LockoutBadCount=$($LockoutBadCount);;;"
+$output = "check_status.$($states_text[$state])::condition==$errmsg | errlvl=$state;;;"
 $host.ui.RawUI.ForegroundColor = $($state_colors[$state])
 Write-Output $output
 $host.ui.RawUI.ForegroundColor = $t
