0c3ebcae1a273abb4296c944c7e4ef0cdf5d7327
Alexander Shevchuk <79712453+Anrikko@users.noreply.github.com>
2022-07-07 20:03:47 +0300

    Fixed output as an object per string

--- check_scripts/check_db_status.ps1
+++ check_scripts/check_db_status.ps1
@@ -8,22 +8,23 @@ $t = $host.ui.RawUI.ForegroundColor
 $states_text = @('ok', 'warning', 'critical', 'unknown')
 $state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
 $state = 0
-$list_db = @()
-$bad_db
+$list_db = ""
+$bad_db = ""
 $n = 0
-$count
+$count = 0
 
 try
 {
 	[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | out-null
 	$s = New-Object ('Microsoft.SqlServer.Management.Smo.Server') "$servername"
-	$dbs=$s.Databases
+	$dbs = $s.Databases
 	$count = $dbs.Count
 	foreach ($db in $dbs)
 	{
 		if (!($db.Status -like "*Normal*"))
 		{
-			$list_db += $db.Name + " - " + $db.Status
+            $s = $db.Name + " - " + $db.Status
+			$list_db += $s.ToString()
 			$n++
 			$state = 2
 		}
@@ -37,8 +38,8 @@ catch
 	$state = 3
 }
 
-$output = "check_db_status.$($states_text[$state])::bad_db==$bad_db | count_bad=$n;;; count_all=$count;;;"
+$output = "check_db_status.$($states_text[$state])::bad_db==$bad_db | count_bad=$n;1;1;0; count_all=$count;1;1;0;" 
 $host.ui.RawUI.ForegroundColor = $($state_colors[$state])
-Write-Output $output
+Write-Output $output.ToString()
 $host.ui.RawUI.ForegroundColor = $t
-exit $state 
+exit $state
