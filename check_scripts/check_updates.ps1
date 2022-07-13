8d891f4efe17dee6e3526fb68b1054975b4dcc1e
Alexander Shevchuk <79712453+Anrikko@users.noreply.github.com>
2022-07-13 11:54:39 +0300

    Changed the method for getting the date of the last update

--- check_scripts/check_updates.ps1
+++ check_scripts/check_updates.ps1
@@ -1,4 +1,4 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 Param(
   [Parameter()]
    [int32]$W = 30,
@@ -14,34 +14,46 @@ $state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
 $state = 3
 
 # latest update installation date and number of available updates
-try {
-    $UpdateSession = New-Object -ComObject Microsoft.Update.Session
-    $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
-    $SearchResult = $UpdateSearcher.Search("IsAssigned=1 and IsHidden=0 and IsInstalled=0")
-    $availableUpdates = $($SearchResult.updates.count)
-          
-    $lastUpdate = (New-Object -com "Microsoft.Update.AutoUpdate").Results.LastInstallationSuccessDate
-    $daysSinceLastUpdate = (New-TimeSpan –Start $lastUpdate –End (Get-Date)).Days
+try
+{
+	$Session = New-Object -ComObject Microsoft.Update.Session
+	$Searcher = $Session.CreateUpdateSearcher()
+	$SearchResult = $Searcher.Search("IsAssigned=1 and IsHidden=0 and IsInstalled=0")
+	$availableUpdates = $($SearchResult.updates.count)
+	$HistoryCount = $Searcher.GetTotalHistoryCount()
+	$lastUpdate = $Searcher.QueryHistory(0,$HistoryCount) | ForEach-Object -Process {
+		New-Object -TypeName PSObject -Property @{
+			InstalledOn = Get-Date -Date $_.Date;
+		}
+	} | Sort-Object -Descending -Property InstalledOn | select -first 1
+
+    $daysSinceLastUpdate = (New-TimeSpan –Start $lastUpdate.InstalledOn –End (Get-Date)).Days
     
-    if($daysSinceLastUpdate -gt $W -and $availableUpdates) {
-        $state=2
-    } else {
-        $state=0
+    if ($daysSinceLastUpdate -gt $W -and $availableUpdates)
+	{
+        $state = 2
+    }
+	else
+	{
+        $state = 0
     }
 }
-catch { 
+catch
+{ 
     Write-Host $_ -ForegroundColor Red
     $state = 3  
 }
 
 $output = "check_updates.$($states_text[$state])::availableupdates==$($availableUpdates)__dayssincelastupdate==$daysSinceLastUpdate | availableupdates=$availableupdates;;; dayssincelastupdate=$daysSinceLastUpdate;;;"
 
-if ($host.UI.RawUI.WindowTitle -match 'Powershell') {
+if ($host.UI.RawUI.WindowTitle -match 'Powershell')
+{
     $host.ui.RawUI.ForegroundColor = $($state_colors[$state])
 }
 
 Write-Output $output
-if ($host.UI.RawUI.WindowTitle -match 'Powershell') {
+if ($host.UI.RawUI.WindowTitle -match 'Powershell')
+{
     $host.ui.RawUI.ForegroundColor = $t
 }
 
