ebb6f8aeee881321a8c3198b155e8d92e19c705e
Alexander Shevchuk <79712453+Anrikko@users.noreply.github.com>
2022-08-04 18:24:33 +0300

    Changed execution logic
    Now the check is not done if the backup task is running, you can also check several tasks, the code with the task check did not work, fixed it

--- check_scripts/check_dirs_compare.ps1
+++ check_scripts/check_dirs_compare.ps1
@@ -1,49 +1,69 @@
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory=$true)][string]$source,
-        [Parameter(Mandatory=$true)][string]$destination,
-        [Parameter()][string]$backupTask
-    )
+[CmdletBinding()]
+Param
+(
+	[Parameter(Mandatory=$true)][string]$source,
+	[Parameter(Mandatory=$true)][string]$destination,
+	[Parameter()][string[]]$backupTasks
+)
 
-    Begin {
-        $output = $null
-        $diff = $null
-        $t = $host.ui.RawUI.ForegroundColor
-        $states_text = @('ok', 'warning', 'critical', 'unknown','task_is_active')
-        $state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
-        $state = 3
-        $state2 = 3
-    }
+Begin
+{
+	$output = $null
+	$diff = $null
+	$t = $host.ui.RawUI.ForegroundColor
+	$states_text = @('ok', 'warning', 'critical', 'unknown','task_is_active')
+	$state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
+	$state = 3
+	$state2 = 3
+	[bool]$backup = $false
+}
 
-    Process {
-        try { 
-            $fso = Get-ChildItem -Recurse -path $source
-            $fsoBU = Get-ChildItem -Recurse -path $destination
-            $cmp = Compare-Object -ReferenceObject $fso -DifferenceObject $fsoBU
-            $diff = $cmp.InputObject.Count
-            if($diff -eq 0) {$state=0}
-            else {$state=2}
-            $state2=$state
-            if($backupTask) {
-                $task = Get-ScheduledTask -TaskName $backupTask
-                if($task.State -eq 'Running') {
-                    $state=0
-                    $state2=4
-                    Write-Host 'Dirs are different, but the task is still running' -ForegroundColor Yellow
+Process
+{
+	try
+	{
+		foreach($backupTask in $backupTasks)
+		{
+			if ($backupTask.State -eq 'Running')
+			{
+				$state=0
+				$state2=4
+				Write-Host 'Dirs are different, but the task is still running' -ForegroundColor Yellow
+				$backup = $true
+			}
+		}
+		
+		if (!$backup)
+		{
+			$fso = Get-ChildItem -Recurse -path $source
+			$fsoBU = Get-ChildItem -Recurse -path $destination
+			$cmp = Compare-Object -ReferenceObject $fso -DifferenceObject $fsoBU
+			$diff = $cmp.InputObject.Count
+			
+			if($diff -eq 0)
+			{
+				$state = 0
+			}
+			else
+			{
+				$state = 2
+			}
+			
+			$state2=$state
+		}
+	}
+	catch
+	{
+		Write-Host $_ -ForegroundColor Red
+	}
+}
 
-                }
-            }    
-        }
-        catch {
-            Write-Host $_ -ForegroundColor Red
-        }
-    }
-
-    End {
-        $output = "check_dirs_compare.$($states_text[$state2]) | diff=$diff;;;"
-        Write-Verbose $output
-        $host.ui.RawUI.ForegroundColor = $($state_colors[$state])
-        Write-Output $output
-        $host.ui.RawUI.ForegroundColor = $t
-        exit $state
-    }
+End
+{
+	$output = "check_dirs_compare.$($states_text[$state2]) | diff=$diff;;;"
+	Write-Verbose $output
+	$host.ui.RawUI.ForegroundColor = $($state_colors[$state])
+	Write-Output $output
+	$host.ui.RawUI.ForegroundColor = $t
+	exit $state
+}
