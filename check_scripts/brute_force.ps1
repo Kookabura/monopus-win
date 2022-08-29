cc54161826ee352a2a1a0ef762a97c2755492b8b
Alexander Shevchuk <79712453+Anrikko@users.noreply.github.com>
2022-08-29 18:07:34 +0300

    Added a filter to the check

--- check_scripts/brute_force.ps1
+++ check_scripts/brute_force.ps1
@@ -14,7 +14,7 @@ $state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
 $state = 3
 
 try {
-    $attempts = (Get-WinEvent -FilterHashtable @{logname='security'; StartTime=$(Get-Date).AddMinutes(('-' + $period)); keywords=4503599627370496} -ErrorAction SilentlyContinue |  Measure-Object -Sum -Property Id -ErrorAction Stop).Count
+    $attempts = (Get-WinEvent -FilterHashtable @{logname='security'; StartTime=$(Get-Date).AddMinutes(('-' + $period)); keywords=4503599627370496; ID=4625} -ErrorAction SilentlyContinue |  Measure-Object -Sum -Property Id -ErrorAction Stop).Count
     if ($w -and $attempts -ge $w -and $attempts -lt $c) {
         $state = 1
     } elseif ($c -and $attempts -ge $c) {
