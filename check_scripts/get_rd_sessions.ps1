92a721408679813372696b3a2094bf472cdf9245
Anrikko <sanek.for.games@gmail.com>
2022-10-26 14:11:12 +0300

    Updated script

--- check_scripts/get_rd_sessions.ps1
+++ check_scripts/get_rd_sessions.ps1
@@ -10,11 +10,20 @@ $states_text = @('ok', 'warning', 'critical', 'unknown')
 $state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
 $state = 0
 
-$sessions = (Get-RDUserSession  | measure-object).Count
-$perf = "active_sessions=" + $sessions + ";0;0;0;"
+try
+{
+    $sessions = (Get-RDUserSession  | measure-object).Count
+    if ($sessions -eq 0)
+    {
+        $sessions = (query session | measure-object).Count
+    }
+}
+catch
+{
+    $state = 3
+}
 
-
-$output = "get_rd_sessions_$($states_text[$state])::$output | $perf"
+$output = "get_rd_sessions_$($states_text[$state])::$output | active_sessions=$sessions;0;0;0;"
 Write-Verbose $output
 $host.ui.RawUI.ForegroundColor = $($state_colors[$state])
 Write-Output $output
