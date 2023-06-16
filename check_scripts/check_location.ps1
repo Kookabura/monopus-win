2b6183b98338a163d573db8633971c0d8b3a1b90
belkovda <belkovda@gmail.com>
2023-06-16 18:50:16 +0300

    Added new script check_location

---
+++ check_scripts/check_location.ps1
@@ -0,0 +1,26 @@
+﻿[CmdletBinding()]
+param(
+    [Parameter(ValueFromPipeline)]
+    [AllowEmptyString()]
+    [string]$country_code
+)
+
+$states_text = @('ok', 'warn')
+$state = 1
+
+if ([string]::IsNullOrEmpty($country_code)) {
+    $status = 'ArgMiss'
+} else {
+		$status = 'ArgOk'
+    $code = (Invoke-WebRequest http://ifconfig.io/country_code).Content
+    if ($code.Trim() -eq $country_code.Trim()) {
+			$state = 0
+		}
+}
+
+$output = "location.$($states_text[$state]).$status"
+
+Write-Output $output
+exit $state
+
+
