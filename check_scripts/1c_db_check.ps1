76c64beaba4d28409b27513866b9d1feea281f30
oseminikhin <oseminikhin@tolkit.top>
2023-09-11 17:49:20 +0300

    check_scripts/1c_db_check.ps1 updated

--- check_scripts/1c_db_check.ps1
+++ check_scripts/1c_db_check.ps1
@@ -2,7 +2,8 @@
 Param(
     [Parameter()][float]$W = 0.8,
     [Parameter()][float]$C = 1,
-    [Parameter()][int32]$period = 10
+    [Parameter()][int32]$period = 10,
+    [Parameter(Mandatory=$true)][string]$server
 )
 
 $t = $host.ui.RawUI.ForegroundColor
@@ -10,19 +11,21 @@ $states_text = @('ok', 'warning', 'critical', 'unknown')
 $state_colors = @('Green', 'Yellow', 'Red', 'DarkGray')
 
 $platform1c_obj = "V83.COMConnector"
-#$service1c_name =  "1C:Enterprise 8.3 Server Agent*"
-$agent1c_connection = "192.168.10.11"   #192.168.10.10/bd_test, 192.168.10.11/bd_test
-#$ErrorActionPreference = "SilentlyContinue"
 $state = 0
 
 $perfdata = ""
 
 try
 {
-    #regsvr32 "c:\Program Files\1cv8\8.3.19.1264\bin\comcntr.dll" IInfoBaseConnectionInfo.durationAllDBMS
-
-	$comobj1c = New-Object -ComObject $platform1c_obj			#Создаем COM объект 1С
-	$connect1c = $comobj1c.ConnectAgent($agent1c_connection)	#Подключаемя к агенту сервера 1С
+	try {
+        $comobj1c = New-Object -ComObject $platform1c_obj			#Создаем COM объект 1С
+    } catch {
+        $comDllPath = Get-ChildItem -Path "c:\Program Files" -Filter "comcntr.dll" -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.FullName -notmatch 'C:\\Program Files\\Microsoft Azure Recovery Services Agent\\Scratch\\SSBV' } | Select-Object -ExpandProperty FullName -last 1
+        $regsvr32Output = regsvr32.exe /s $comDllPath           # Если библиотека не зарегистрирована - регистрируем её
+        $comobj1c = New-Object -ComObject $platform1c_obj
+    }
+    			
+	$connect1c = $comobj1c.ConnectAgent($server)	#Подключаемя к агенту сервера 1С
 
 
 	$cluster1c = $connect1c.GetClusters()						#Получаем доступные кластеры на данном сервере
@@ -55,3 +58,4 @@ $host.ui.RawUI.ForegroundColor = $($state_colors[$state])
 Write-Output $output
 $host.ui.RawUI.ForegroundColor = $t
 exit $state
+
